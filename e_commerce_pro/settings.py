e8f975ffc78a3a32d3f4213a9fe6c8f1aecf0222
John Doe <johndoe@example.com>
2025-04-15 11:40:49 +0600

    cofigure django admin

--- e_commerce_pro/settings.py
+++ e_commerce_pro/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,3 +132,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    'site_header': "Amar Shop",
+    'site_brand': "Amar Shop",
+    'site_logo': "asset/img/theme/img1.gif",
+    'copyright': "Amar-Shop.com",
+
+}
